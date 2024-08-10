96f9b5be5a5f17f0dd65f87df22bf471ea7170ec
uaekinf <uaekinf@gmail.com>
2024-08-10 19:19:31 +0000

    7

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
-import express from 'express';
-import { createTransport } from 'nodemailer';
-import cors from 'cors';
-import pkg from 'body-parser';
-const { json } = pkg;
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(json());
+app.use(bodyParser.json());
 
-const transporter = createTransport({
+const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'erhamashamsi@gmail.com',
-        pass: 'System.out.print(hello);'
+        user: 'your_email@gmail.com',
+        pass: 'your_password'
     }
 });
 
 app.post('/send-email', (req, res) => {
-    console.log('Received request:', req.body);
     const { email, lastName, message } = req.body;
-
     const mailOptions = {
-        from: 'erhamashamsi@gmail.com',
+        from: 'your_email@gmail.com',
         to: email,
         subject: 'Contact Form Submission',
         text: `Message from ${lastName}: ${message}`
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
+    transporter.sendMail(mailOptions, function(error, info){
         if (error) {
             console.log(error);
             res.status(500).send('Error sending email');
--- src/Contact Us/EmailContext.jsx
+++ src/Contact Us/EmailContext.jsx
@@ -8,7 +8,7 @@ export const EmailProvider = ({ children }) => {
     const sendEmail = async (emailData) => {
         console.log('Sending data:', emailData);
             try {
-                const response = await fetch('https://3001-idx-hama-1723052994590.cluster-p6qcyjpiljdwusmrjxdspyb5m2.cloudworkstations.dev/send-email', {
+                const response = await fetch('https://3002-idx-hama-1723052994590.cluster-p6qcyjpiljdwusmrjxdspyb5m2.cloudworkstations.dev//send-email', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
